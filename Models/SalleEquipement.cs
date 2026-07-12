0fc36f4317d3ed78a7eba264fc95b9a76a1b721f
irielamatthieu <irielamatthieu@gmail.com>
2026-07-12 23:26:15 +0300

    model

---
+++ Models/SalleEquipement.cs
@@ -0,0 +1,15 @@
+﻿namespace EMIT_EDT.Models
+{
+    public class SalleEquipement
+    {
+        public int SalleId { get; set; }
+        public Salle? Salle { get; set; }
+
+        public int EquipementId { get; set; }
+        public Equipement? Equipement { get; set; }
+
+        public int Quantite { get; set; } = 1;
+
+        public string Etat { get; set; } = "Fonctionnel";
+    }
+}
---
+++ Models/Utilisateur.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EMIT_EDT.Models
+{
+    public class Utilisateur : IdentityUser
+    {
+        public string Nom { get; set; } = "";
+        public string Prenom { get; set; } = "";
+        public string Role { get; set; } = "Etudiant";
+        public string? Matricule { get; set; }
+        public string? PhotoPath { get; set; }
+        public bool Actif { get; set; } = true;
+        public DateTime DateCreation { get; set; } = DateTime.Now;
+        public DateTime? DerniereConnexion { get; set; }
+
+        // ✅ AJOUTE CETTE LIGNE
+        public Enseignant? Enseignant { get; set; }
+    }
+}
